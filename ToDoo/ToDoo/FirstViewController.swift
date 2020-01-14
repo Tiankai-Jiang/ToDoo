e1bddb0f18e4eb7bbad168645dafa36f1cee6667
tiankai <tiankai@v1040-wn-rt-c-69-110.campus-dynamic.uwaterloo.ca>
2020-01-14 14:32:09 -0500

    create empty project

---
+++ ToDoo/ToDoo/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ToDoo
+//
+//  Created by tiankai on 2020-01-14.
+//  Copyright © 2020 tiankai. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ToDoo/ToDoo/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ToDoo
+//
+//  Created by tiankai on 2020-01-14.
+//  Copyright © 2020 tiankai. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
