2304c55ff5b86efc0e027ef1fe367f8a11c18f2b
tiankai <tiankai@v1040-wn-rt-c-91-125.campus-dynamic.uwaterloo.ca>
2020-01-15 10:56:39 -0500

    add addHabit scene

---
+++ ToDoo/ToDoo/Controllers/AddHabitViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddHabitViewController.swift
+//  ToDoo
+//
+//  Created by tiankai on 2020-01-15.
+//  Copyright © 2020 tiankai. All rights reserved.
+//
+
+import UIKit
+
+class AddHabitViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
