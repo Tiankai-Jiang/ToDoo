91cad75e16ecbb75fbd55dcc7cb8d32de314a4df
tiankai <tiankai@v1040-wn-rt-c-89-116.campus-dynamic.uwaterloo.ca>
2020-02-06 08:43:36 -0500

    disable scrolling in tableview

--- ToDoo/ToDoo/Controllers/CalendarViewController.swift
+++ ToDoo/ToDoo/Controllers/CalendarViewController.swift
@@ -140,21 +140,3 @@ extension CalendarViewController: CalendarViewDelegate {
        }
     
 }
-
-extension Date {
-
-    // Convert local time to UTC (or GMT)
-    func toGlobalTime() -> Date {
-        let timezone = TimeZone.current
-        let seconds = -TimeInterval(timezone.secondsFromGMT(for: self))
-        return Date(timeInterval: seconds, since: self)
-    }
-
-    // Convert UTC (or GMT) to local time
-    func toLocalTime() -> Date {
-        let timezone = TimeZone.current
-        let seconds = TimeInterval(timezone.secondsFromGMT(for: self))
-        return Date(timeInterval: seconds, since: self)
-    }
-
-}
--- ToDoo/ToDoo/Utility.swift
+++ ToDoo/ToDoo/Utility.swift
@@ -58,3 +58,21 @@ extension UIViewController {
         view.endEditing(true)
     }
 }
+
+extension Date {
+
+    // Convert local time to UTC (or GMT)
+    func toGlobalTime() -> Date {
+        let timezone = TimeZone.current
+        let seconds = -TimeInterval(timezone.secondsFromGMT(for: self))
+        return Date(timeInterval: seconds, since: self)
+    }
+
+    // Convert UTC (or GMT) to local time
+    func toLocalTime() -> Date {
+        let timezone = TimeZone.current
+        let seconds = TimeInterval(timezone.secondsFromGMT(for: self))
+        return Date(timeInterval: seconds, since: self)
+    }
+
+}
