ac2530be6fceaa4d9541cb1db1a71b286dcd59f5
tiankai <tiankai@tiankais-MacBook-Pro.local>
2020-02-10 09:44:16 -0500

    added calendar in habit detail scene

--- ToDoo/ToDoo/Controllers/HabitDetailViewController.swift
+++ ToDoo/ToDoo/Controllers/HabitDetailViewController.swift
@@ -1,7 +1,10 @@
 import UIKit
+import JTAppleCalendar
 
 class HabitDetailViewController: UIViewController {
 
+    @IBOutlet weak var calendar: JTAppleCalendarView!
+    
     @IBOutlet weak var tableView: UITableView!
     
     var habitInformation: [HabitInfo] = []
@@ -10,7 +13,14 @@ class HabitDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        calendar.scrollDirection = .horizontal
+        calendar.scrollingMode   = .stopAtEachCalendarFrame
+        calendar.showsHorizontalScrollIndicator = false
+        
+        calendar.register(UINib(nibName: "DateCell", bundle: nil), forCellWithReuseIdentifier: "dateCell")
+        
+        calendar.register(UINib(nibName: "DateHeader", bundle: nil), forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "dateHeader")
+        
         tableView.register(UINib(nibName: K.Cells.habitDetailXib, bundle: nil), forCellReuseIdentifier: K.Cells.habitDetailCell)
     }
     
@@ -39,3 +49,80 @@ extension HabitDetailViewController: UITableViewDelegate{
     
 }
 
+extension HabitDetailViewController: JTAppleCalendarViewDataSource {
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy MM dd"
+
+        let startDate = formatter.date(from: "2020 01 01")!
+        let endDate = Date()
+        return ConfigurationParameters(startDate: startDate, endDate: endDate)
+    }
+    func configureCell(view: JTAppleCell?, cellState: CellState) {
+        guard let cell = view as? DateCell  else { return }
+        cell.dateLabel.text = cellState.text
+        
+        handleCellTextColor(cell: cell, cellState: cellState)
+        handleCellSelected(cell: cell, cellState: cellState)
+    }
+    func handleCellTextColor(cell: DateCell, cellState: CellState) {
+        if cellState.dateBelongsTo == .thisMonth {
+            cell.dateLabel.textColor = hexStringToUIColor(hex: "194348")
+            cell.dateLabel.isHidden = false
+            cell.selectedView.isHidden = false
+        } else {
+            //cell.dateLabel.textColor = UIColor.gray
+            cell.dateLabel.isHidden = true
+            cell.selectedView.isHidden = true
+        }
+        
+        cell.dateLabel.textColor = hexStringToUIColor(hex: Calendar.current.isDateInWeekend(cellState.date) ? "f15c5c" : "194348")
+    }
+    func handleCellSelected(cell: DateCell, cellState: CellState) {
+        if cellState.isSelected {
+            cell.selectedView.layer.cornerRadius = cell.selectedView.bounds.width / 2
+            cell.selectedView.backgroundColor = hexStringToUIColor(hex: "c6f1d6")
+        } else {
+            cell.selectedView.layer.cornerRadius = cell.selectedView.bounds.width / 2
+            cell.selectedView.backgroundColor = .white
+        }
+        let colorList=["1fab89","ff8080","ffba92"]
+        cell.selectedView.backgroundColor = hexStringToUIColor(hex: colorList[Int.random(in: 0...2)])
+        cell.dateLabel.textColor = .white
+    }
+}
+
+extension HabitDetailViewController: JTAppleCalendarViewDelegate {
+    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "dateCell", for: indexPath) as! DateCell
+        self.calendar(calendar, willDisplay: cell, forItemAt: date, cellState: cellState, indexPath: indexPath)
+        return cell
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
+        configureCell(view: cell, cellState: cellState)
+    }
+    
+    
+    func calendar(_ calendar: JTAppleCalendarView, headerViewForDateRange range: (start: Date, end: Date), at indexPath: IndexPath) -> JTAppleCollectionReusableView {
+        let formatter = DateFormatter()  // Declare this outside, to avoid instancing this heavy class multiple times.
+        formatter.dateFormat = "MMMM yyyy"
+        
+        let header = calendar.dequeueReusableJTAppleSupplementaryView(withReuseIdentifier: "dateHeader", for: indexPath) as! DateHeader
+        header.monthTitle.text = formatter.string(from: range.start)
+        return header
+    }
+    func calendar(_ calendar: JTAppleCalendarView, didSelectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
+        configureCell(view: cell, cellState: cellState)
+    }
+
+    func calendar(_ calendar: JTAppleCalendarView, didDeselectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
+        configureCell(view: cell, cellState: cellState)
+    }
+    func calendar(_ calendar: JTAppleCalendarView, shouldSelectDate date: Date, cell: JTAppleCell?, cellState: CellState) -> Bool {
+        return true // Based on a criteria, return true or false
+    }
+    func calendarSizeForMonths(_ calendar: JTAppleCalendarView?) -> MonthSize? {
+        return MonthSize(defaultSize: 50)
+    }
+}
