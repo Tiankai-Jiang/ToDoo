96d7da5e367b12ef4970e74a71509733a80ea6a9
tiankai <tiankai@tiankais-MacBook-Pro.local>
2020-02-28 23:41:05 -0500

    changed toast position

--- ToDoo/ToDoo/Controllers/RegisterViewController.swift
+++ ToDoo/ToDoo/Controllers/RegisterViewController.swift
@@ -12,7 +12,6 @@ class RegisterViewController: UIViewController {
             Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
                 if let e = error{
                     self.view.makeToast(e.localizedDescription, duration: 2.5, position: .center)
-                    
                 }else{
                     self.performSegue(withIdentifier: K.registerSegue, sender: self)
                 }
--- ToDoo/ToDoo/Controllers/WelcomeViewController.swift
+++ ToDoo/ToDoo/Controllers/WelcomeViewController.swift
@@ -14,8 +14,6 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
 }
