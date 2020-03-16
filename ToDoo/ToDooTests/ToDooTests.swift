bd28c53106f742ca9f30ff31677ec12db9f5c79b
apple <625543788@qq.com>
2020-03-16 16:08:42 -0400

    add unit test cases and a test graph

--- ToDoo/ToDooTests/ToDooTests.swift
+++ ToDoo/ToDooTests/ToDooTests.swift
@@ -11,7 +11,7 @@ import XCTest
 
 
 class ToDooTests: XCTestCase {
-
+    let test = HomeViewController()
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -31,5 +31,21 @@ class ToDooTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
+    func testGetIfCheckedArray(){
+        XCTAssertTrue(test.getIfCheckedArray(["1584115201": 1584134002, "1583427601": 1583434543, "1583082001": 1583091393, "1584201601": 1584235321, "1583683201": 1583698862, "1582304401": 1582346843, "1583769601": 1583735499], 1582299129) == [true, false, false, false, false, false, false, false, false, true, false, false, false, true, false, false, true, true, false, false, false, true, true, false, false])
+        XCTAssertTrue(test.getIfCheckedArray(["1584201601": 1584235313, "1582304401": 1582318432, "1583683201": 1583703456, "1583769601": 1583810625, "1583082001": 1583091389, "1583254801": 1583259912, "1582909201": 1582940449], 1582299058) == [true, false, false, false, false, false, false, true, false, true, false, true, false, false, false, false, true, true, false, false, false, false, true, false, false])
+        XCTAssertTrue(test.getIfCheckedArray(["1583769601": 1583810673, "1583683201": 1583700549, "1584201601": 1584240876], 1583700544) == [true, true, false, false, false, false, true, false, false])
+    }
+    func testGetLongestStreak(){
+        XCTAssertTrue(test.getLongestStreak([true,false,false,true, true,true]) == 3)
+        XCTAssertTrue(test.getLongestStreak([true, false, false, false, false, false, false, true, false, true, false, true, false, false, false, false, true, true, false, false, false, false, true, false, false]) == 2)
+        XCTAssertTrue(test.getLongestStreak([true, false, false, false, false, false, false, true, false, true, false, false, false, false, false, false, true, true, false, false, false, true, true, false, false]) == 2)
+         XCTAssertTrue(test.getLongestStreak([true, true, false, false, false, true, true, false, false]) == 2)
+    }
+    func testGetCurrentStreak(){
+        XCTAssertTrue(test.getCurrentStreak([true,false,false,false, true,true]) == 2)
+        XCTAssertTrue(test.getCurrentStreak([true,false,false,false,false,true]) == 1)
+        XCTAssertTrue(test.getCurrentStreak([true, false, false, false, false, false, false, true, false, true, false, false, false, false, false, false, true, true, false, false, false, true, true, false, false]) == 0)
+        XCTAssertTrue(test.getCurrentStreak([true, true, false, false, false, true, true, false, false]) == 0)
+    }
 }
